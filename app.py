7860cded971785cdc92056133cd0bb6ceac49b72
Kenneth Lew <lew1101@192.168.12.23>
2024-05-31 13:52:19 -0400

    rename main to app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from st_pages import Page, show_pages
 
 
-def main() -> None:
+def app() -> None:
     show_pages([
         Page("pages/Home.py", "Home"),
         Page("pages/Task_1.py", "Task 1 - Projectile Motion"),
@@ -17,4 +17,4 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main()
+    app()
