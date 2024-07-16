0cc4f41441e0149c0549fbdf0bc90d955fe6d844
Timothy Ka <141932549+TimothyKa100@users.noreply.github.com>
2024-07-16 08:59:07 +0100

    Update Task_6.py

--- pages/Task_6.py
+++ pages/Task_6.py
@@ -6,7 +6,7 @@ import numpy as np
 import config
 from utils import cache_data_default
 
-st.set_page_config(page_title="Task 6", **config.PAGE_CONFIG)
+st.set_page_config(page_title="Task 4", **config.PAGE_CONFIG)
 config.apply_custom_styles()
 
 # ==================
@@ -22,13 +22,64 @@ model_tab, math_tab, code_tab, = st.tabs(["Model", "Derivations", "Source Code"]
 # CODE
 # =====================
 
-PLOT_DEFAULTS = {}
+PLOT_DEFAULTS = {"theta": 60.0, "g": 9.81, "u": 10.0, "h": 2.0}
 
 with code_tab, st.echo():
 
     @cache_data_default(**PLOT_DEFAULTS)
-    def generate_task_6(*, kwarg):
-        pass
+    def generate_task_6(*, theta: float, g: float, u: float, h: float):
+        from math import sin, cos, sqrt, radians, asin, log, tan
+        
+        def calcDist(rad, x, max = False):
+            if h == 0 and max:
+                return u**2/g*(log(1+sqrt(2))+sqrt(2))/2 
+            elif h==0:
+                return u**2/g*(log((1+sin(rad))/cos(rad))*cos(rad)**2+sin(rad))
+            else:
+                z1 = tan(rad)
+                z2 = tan(rad)-g*x/u**2*(1+tan(rad)**2)
+                calc_z1 = calc_z_part(z1)
+                calc_z2 = calc_z_part(z2)
+                return u**2/g/(1+tan(rad)**2)*(calc_z1-calc_z2)
+        
+        def calc_z_part(z):
+            return 0.5*log(abs(sqrt(1+z**2) + z ) ) + 0.5*z*sqrt( 1+ z**2 )
+
+        fig = go.Figure(layout=config.GO_BASE)\
+            .update_layout(title_text="Arc Length of Projectile Motion (with Analytical Model)", xaxis_title="x (m)",  yaxis_title="y (m)")
+
+        # inputted trajectory
+        rad = radians(theta)
+        ux = u * cos(rad)
+        uy = u * sin(rad)
+
+        total_t = (uy + sqrt(uy**2 + 2 * g * h)) / g
+        range = ux * total_t
+        dist = calcDist(rad, range)
+
+        original_x = np.linspace(0, range, config.GRAPH_SAMPLES)
+        inputted_traj = h + (uy / ux) * original_x - (g / 2 / ux**2) * original_x**2
+
+        # maximize range
+        rad_max = asin(1 / sqrt(2 + 2 * g * h / u**2))
+        ux = u * cos(rad_max)
+        uy = u * sin(rad_max)
+
+        max_range_t = (uy + sqrt(uy**2 + 2 * g * h)) / g
+        range_max = u / g * sqrt(u**2 + 2 * g * h)
+        max_dist = calcDist(rad, range_max, True)
+
+        max_range_x = np.linspace(0, range_max, config.GRAPH_SAMPLES)
+        max_range_traj = h + (uy / ux) * max_range_x - (g / 2 / ux**2) * max_range_x**2
+
+        fig.add_trace(go.Scatter(name="Trajectory", x=original_x, y=inputted_traj, mode="lines", line_shape='spline'))\
+           .add_trace(go.Scatter(name="Max Range", x=max_range_x, y=max_range_traj, mode="lines", line_shape='spline'))\
+           .add_trace(go.Scatter(name="Range", x=[range], y=[0], text=[f"({range:.2f}, {0})"],
+                textposition="top center", textfont=dict(size=14), marker_symbol="x", marker=dict(size=11), mode='markers+text', showlegend=False))\
+           .add_trace(go.Scatter(name="Max. Range", x=[range_max], y=[0], text=[f"({range_max:.2f}, {0})"],
+                textposition="bottom center", textfont=dict(size=14), marker_symbol="x", marker=dict(size=11), mode='markers+text', showlegend=False))\
+
+        return fig, range, total_t, rad_max, range_max, max_range_t, dist, max_dist
 
 
 # =====================
@@ -42,23 +93,49 @@ with model_tab:
         col1, col2 = st.columns(2, gap="large")
 
         with col1:
-            pass
+            theta = st.number_input("Launch Angle (deg)",
+                                    min_value=0.0,
+                                    max_value=90.0,
+                                    value=PLOT_DEFAULTS["theta"])
+            gravity = st.number_input("Gravity (m/s²)", min_value=0.0, value=PLOT_DEFAULTS["g"])
 
         with col2:
-            pass
+            vel = st.number_input("Initial Speed (m/s)", min_value=0.0, value=PLOT_DEFAULTS["u"])
+            height = st.number_input("Height (m)", value=PLOT_DEFAULTS["h"])
 
         submitted = st.form_submit_button("Generate")
 
     try:
-        # fig, *args = generate_task_6()
+        fig, range, total_t, rad_max, range_max, max_range_t, dist, max_dist = generate_task_6(theta=theta,
+                                                                               g=gravity,
+                                                                               u=vel,
+                                                                               h=height)
+
+        from math import degrees
 
         st.write("")
         f"""
         #### Calculated Values
+        
+        **Range**: {range:.3f} m
+        
+        **Flight Time**: {total_t:.3f} s
+        
+        **Distance Travelled by Projectile**: {dist:.3f} m
 
         """
+        st.write("")
+        f"""
+        **Launch Angle of Trajectory Maximizing Range**: {degrees(rad_max):.3f} deg
+        
+        **Maximum Range**: {range_max:.3f} m
+        
+        **Flight Time of Trajectory Maximizing Range**: {max_range_t:.3f} s
+        
+        **Distance Travelled by Projectile With Maximising Range**: {max_dist:.3f} m
+        """
 
-        # st.plotly_chart(fig, **config.plotly_chart_config)
+        st.plotly_chart(fig, **config.PLOTLY_CONFIG)
     except Exception as e:
         st.exception(e)
 
