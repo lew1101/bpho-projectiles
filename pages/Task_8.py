275577c064f4440ae328a53eb437e7ae95387d2f
Kenneth Lew <kennethlew001@gmail.com>
2024-07-22 23:04:54 +0800

    add animation to graph

--- pages/Task_8.py
+++ pages/Task_8.py
@@ -14,8 +14,6 @@ r"""
 ## Task 8 - Bouncing Projectile
 
 **Description:** Use a numerical method assuming constant acceleration motion between small, discrete timesteps (e.g. the "Verlet" method) to compute a projectile trajectory which includes the possibility of a _bounce_. Define the _coefficient of restitution_ $C$ to be the vertical speed of separation divided by the vertical speed of approach. Assume a constant horizontal velocity, and stop the simulation after $N$ bounces. 
-
-**_Extension_**: Modify your code to _animate_ the trajectory, and ideally, create a video file for efficient future playback.
 """
 
 model_tab, math_tab, code_tab, = st.tabs(["Model", "Derivations", "Source Code"])
@@ -24,18 +22,15 @@ model_tab, math_tab, code_tab, = st.tabs(["Model", "Derivations", "Source Code"]
 # CODE
 # =====================
 
-PLOT_DEFAULTS = {"theta": 45.0, "g": 9.81, "u": 20.0, "h": 2.0, "dt": 0.1, "C": 0.7}
+PLOT_DEFAULTS = {"theta": 45.0, "g": 9.81, "u": 6.0, "h": 2.0, "dt": 0.02, "C": 0.7, "N": 6}
 
 with code_tab, st.echo():
 
     @cache_data_default(**PLOT_DEFAULTS)
-    def generate_task_8(*, theta: float, g: float, u: float, h: float, dt: float, C: float):
+    def generate_task_8(*, theta: float, g: float, u: float, h: float, dt: float, C: float, N: int):
         from math import sin, cos, radians
 
-        N_BOUNCES = 6
-
-        fig = go.Figure(layout=config.GO_BASE)\
-                    .update_layout(title_text="Bouncing Projectile", xaxis_title="x (m)",  yaxis_title="y (m)")
+        ANIMATION_STEP = 1
 
         rad = radians(theta)
 
@@ -52,7 +47,7 @@ with code_tab, st.echo():
         dx = ux * dt
 
         bounces = 0
-        while bounces < N_BOUNCES:
+        while bounces < N:
             x = x + dx  # since x acceleration is 0, dx is constant
             y = y + uy * dt
 
@@ -67,7 +62,71 @@ with code_tab, st.echo():
             x_list.append(x)
             y_list.append(y)
 
-        fig.add_trace(go.Scatter(x=x_list, y=y_list, mode="markers+lines", line_shape='spline'))
+        frames_n = len(x_list)
+        animation_speed = dt * 1000 * ANIMATION_STEP  # sec -> ms
+
+        fig = go.Figure(layout=config.GO_BASE,
+                        data=[
+                            go.Scatter(x=x_list, y=y_list, mode="lines", line_shape='spline'),
+                            go.Scatter(x=x_list, y=y_list, mode="lines", line_shape='spline'),
+                        ],
+                        frames=[
+                            go.Frame(data=[
+                                go.Scatter(x=[x_list[i]],
+                                           y=[y_list[i]],
+                                           mode="markers",
+                                           marker=dict(color="red", size=10))
+                            ]) for i in range(0, frames_n, ANIMATION_STEP)
+                        ])
+
+        fig.update_layout(title_text="Bouncing Projectile",
+                          xaxis_title="x (m)",
+                          yaxis_title="y (m)",
+                          hovermode="closest",
+                          width=800,
+                          height=550,
+                          autosize=False,
+                          margin=dict(t=150),
+                          xaxis=dict(range=[0, max(x_list) + 1], autorange=False),
+                          yaxis=dict(range=[0, max(y_list) + 1], autorange=False),
+                          updatemenus=[
+                              dict(type="buttons",
+                                   buttons=[
+                                       dict(label="Play",
+                                            method="animate",
+                                            args=[
+                                                None,
+                                                dict(frame=dict(duration=animation_speed,
+                                                                redraw=False),
+                                                     fromcurrent=True,
+                                                     transition=dict(duration=animation_speed,
+                                                                     easing="quadratic-in-out"))
+                                            ]),
+                                       dict(label="Pause",
+                                            method="animate",
+                                            args=[[None],
+                                                  dict(frame=dict(duration=0, redraw=False),
+                                                       mode="immediate",
+                                                       transition=dict(duration=0))])
+                                   ],
+                                   direction="left",
+                                   pad=dict(r=10, t=10),
+                                   xanchor="left",
+                                   yanchor="top",
+                                   x=0.07,
+                                   y=1.18,
+                                   showactive=False)
+                          ],
+                          annotations=[
+                              dict(text="Animate:",
+                                   showarrow=False,
+                                   x=0,
+                                   y=1.15,
+                                   font=dict(size=18),
+                                   yref="paper",
+                                   align="right")
+                          ],
+                          showlegend=False)
 
         return fig, total_t
 
@@ -92,6 +151,10 @@ with model_tab:
                                  min_value=0.0,
                                  value=PLOT_DEFAULTS["dt"],
                                  step=0.01)
+            n_bounces = st.number_input("Number of Bounces",
+                                        min_value=1,
+                                        max_value=10,
+                                        value=PLOT_DEFAULTS["N"])
 
         with col2:
             vel = st.number_input("Initial Speed (m/s)", min_value=0.0, value=PLOT_DEFAULTS["u"])
@@ -104,7 +167,13 @@ with model_tab:
         submitted = st.form_submit_button("Generate")
 
     try:
-        fig, total_t = generate_task_8(theta=theta, g=gravity, u=vel, h=height, dt=dt, C=coeff)
+        fig, total_t = generate_task_8(theta=theta,
+                                       g=gravity,
+                                       u=vel,
+                                       h=height,
+                                       dt=dt,
+                                       C=coeff,
+                                       N=n_bounces)
 
         st.write("")
         f"""
