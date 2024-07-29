e6c5bad61ab32a03bdd5b18fb189bdcf05d65385
Kenneth Lew <kennethlew001@gmail.com>
2024-07-29 20:03:22 +0800

    add math derivations for task 3

--- pages/Task_3.py
+++ pages/Task_3.py
@@ -13,7 +13,7 @@ config.apply_custom_styles()
 r"""
 ## Task 3 - Hitting a Target 
 
-**Description:** Create a new projectile model which is based upon calculating trajectories that are launched from $(0, h)$ and pass through a fixed position $(x, y)$. Calculate the minimum launch speed to achieve this, and hence determine "low ball" and "high ball" trajectories.
+**Description:** Create a new projectile model which is based upon calculating trajectories that are launched from $(0, h)$ and pass through a fixed position $(X, Y)$. Calculate the minimum launch speed to achieve this, and hence determine "low ball" and "high ball" trajectories.
 """
 
 model_tab, math_tab, code_tab, = st.tabs(["Model", "Derivations", "Source Code"])
@@ -149,6 +149,122 @@ with model_tab:
 
 with math_tab:
     r"""
+    ##### Finding the High Ball and Low Ball Trajectories
+    
+    Recall that the trajectory of the projectile is given by the function: 
+    
+    $$
+    \begin{equation}
+        y = h + x\tan{\theta} + \frac{g}{2u^2}\left(1+\tan^2{\theta}\right)x^2
+    \end{equation}
+    $$
+    
+    Let $(X, Y)$ represent the coordinates of the fixed target. Given that the initial launch velocity $u$ is known, valid launch angle(s) can be found using the equation 1:
+    
+    $$
+        \begin{align}
+        Y &= h + X\tan{\theta} + \frac{g}{2u^2}\left(1+\tan^2{\theta}\right)X^2 \notag \\
+        \Rightarrow 0 &= \frac{g}{2u^2}X^2\tan^2{\theta} - X\tan{\theta} + Y - h + \frac{g}{2u^2}X^2
+        \end{align}
+    $$
+    
+    We can then solve for $\tan{\theta}$ using the quadratic formula, where:
+    
+    $$
+    \begin{align*}
+        a &= \frac{g}{2u^2}X^2 \\
+        b &= -X \\ 
+        c &= Y - h + \frac{g}{2u^2}X^2
+    \end{align*}
+    $$
+    
+    Thus:
+    
+    $$
+    \begin{align*}
+        \tan{\theta} &= \frac{-b \pm \sqrt{b^2-4ac}}{2a} \\
+        &= \frac{-(-X) \pm \sqrt{(-X)^2-4\left(\frac{g}{2u^2}X^2\right)\left(Y - h + \frac{g}{2u^2}X^2\right)}}{2\frac{g}{2u^2}X^2} 
+    \end{align*}
+    $$
+    
+    Isolating for $\theta$:
+    
+    $$
+    \begin{align}
+        \Rightarrow \theta &= \tan^{-1}\left(\frac{X \pm \sqrt{X^2-\frac{2g}{u^2}X^2\left(Y - h + \frac{g}{2u^2}X^2\right)}}{\frac{g}{u^2}X^2}\right) \notag \\
+        &= \boxed{\tan^{-1}\left(\frac{u^2 \pm u\sqrt{u^2-2g\left(Y - h + \frac{g}{2u^2}X^2\right)}}{gX}\right)}
+    \end{align}
+    $$
+    
+    The above equation can yield at most two solutions, given that the launch velocity is sufficient. The solution with the greater $\theta$ is considered the high ball trajectory, while the one with the smaller $\theta$ is the low ball trajectory.
+    
+    ##### Finding the Minimum Launch Trajectory
+    
+    By considering that the discriminant $b^2 - 4ac$ in equation 3 must be greater than zero to yield real solutions of $\theta$, the range of possible launch velocity $u$ (and consequently the minimum launch velocity) can be found.
+    
+    $$
+    \begin{align*}
+        X^2-4\left(\frac{gX^2}{2u^2}\right)\left(Y - h + \frac{gX^2}{2u^2}\right) &\geq 0\\
+        \Rightarrow 1-\frac{2g}{u^2}\left(Y-h\right)-\frac{g^2X^2}{u^4} &\geq 0 \\
+        \Rightarrow u^4-2gu^2(Y-h)-g^2X^2 &\geq 0
+    \end{align*}
+    $$
+    
+    Then, completing the square:
+    
+    $$
+    \begin{gather*}
+        \Rightarrow \left(u^2 - g(Y-h)\right)^2 - g^2(Y-h)^2 -g^2X^2 \geq 0 \\
+        \Rightarrow \left(u^2 - g(Y-h)\right)^2 \geq g^2(Y-h)^2 + g^2X^2
+    \end{gather*}
+    $$
+
+    Taking the square root on both sides:
+    
+    $$
+    \begin{equation*}
+        \Rightarrow u^2 - g(Y-h) \geq g\sqrt{X^2 + (Y-h)^2}
+    \end{equation*}
+    $$
+    
+    Note that only the principal root is taken, because $u$ must be real and positive. 
+    
+    $$
+    \begin{align}
+        \Rightarrow u^2 &\geq g\left((Y-h) + \sqrt{X^2 + (Y-h)^2}\right) \notag \\
+        \Rightarrow u &\geq \boxed{\sqrt{g}\sqrt{(Y-h) + \sqrt{X^2 + (Y-h)^2}}}
+    \end{align}
+    $$
+    
+    The launch angle of the minimum $u$ trajectory can be found by replacing the minimum solution for $u$ in equation 2 for $u$. Recognizing that $b^2-4ac=0$ for the minimum velocity $u$:
+    
+    $$
+    \begin{align}
+            \tan{\theta} &= \frac{-b}{2a} \notag \\
+            &= \frac{X}{\frac{g}{u^2}X^2} \notag \\
+            &= \frac{u^2}{gX} \notag \\
+            &= \frac{(Y-h) + \sqrt{X^2 + (Y-h)^2}}{X}
+    \end{align}
+    $$
+    $$
+    \begin{equation}
+        \therefore \theta = \boxed{\tan^{-1}{\frac{(Y-h) + \sqrt{X^2 + (Y-h)^2}}{X}}}
+    \end{equation}
+    $$
+    
+    Replacing for $\tan{\theta}$ in equation 1, we can find the equation of the minimum $u$ trajectory:
+    
+    $$
+    \begin{align}
+        y &= h + x\tan{\theta} + \frac{g}{2u^2}\left(1+\tan^2{\theta}\right)x^2 \notag \\
+        &= h + x\left(\frac{Y - h + \sqrt{X^2 + (Y-h)^2}}{X}\right) + \frac{g}{2g\left(Y - h + \sqrt{X^2 + (Y-h)^2}\right)}\left(1+\frac{\left(Y - h + \sqrt{X^2 + (Y-h)^2}\right)^2}{X^2}\right)x^2 \notag \\
+        &= h + x\left(\frac{Y - h + \sqrt{X^2 + (Y-h)^2}}{X}\right) + \frac{1}{2\left(Y - h + \sqrt{X^2 + (Y-h)^2}\right)}\left(\frac{X^2 + (Y-h)^2 + 2(Y-h)\sqrt{X^2+(Y-h)^2} + X^2 + (Y-h)^2}{X^2}\right)x^2 \notag \\
+        &= h + x\left(\frac{Y - h + \sqrt{X^2 + (Y-h)^2}}{X}\right) + \frac{\sqrt{X^2 + (Y-h)^2}}{Y - h + \sqrt{X^2 + (Y-h)^2}}\left(\frac{Y - h + \sqrt{X^2+(Y-h)^2}}{X^2}\right)x^2 \notag \\
+        &= \boxed{h + x\left(\frac{Y - h + \sqrt{X^2 + (Y-h)^2}}{X}\right) + \left(\frac{\sqrt{X^2+(Y-h)^2}}{X^2}\right)x^2} \\
+    \end{align}
+    $$
+    
+    
     """
 
 st.divider()
